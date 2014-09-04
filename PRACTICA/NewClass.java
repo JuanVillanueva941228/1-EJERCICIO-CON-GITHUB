8693742bbb33cc6bf274c97c6fd60b830c0e351f
USUARIO <USUARIO@USER>
2014-09-04 15:53:48 -0500

    Mis primeros archivos subidos

---
+++ PRACTICA/NewClass.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author USUARIO
+ */
+public class NewClass {
+    
+}
+public static void main  men(String [] arguments {
+System.out.println ("Hola Mundo");
+
+}
